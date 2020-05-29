cd23efa4060e3ec646b62e5379799c0f0fa8a54d
jcgsr <cabralguerra@gmail.com>
2020-05-29 19:36:52 -0300

    1st commit

---
+++ rotaautenticar/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <Routes />
+     );
+  }
+}
+ 
+export default App;
